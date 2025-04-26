4400cfff2f7ed897bf860df14db266290b5ac985
Lautaro <lautarotarantini@gmail.com>
2025-04-25 21:22:45 -0300

    desafio 2

---
+++ assets/index.js
@@ -0,0 +1,37 @@
+//alert("hola mundo")
+
+/*
+console.log("hola desde index.js")
+
+const nombreInstructor = "Lautaro"
+const apellidoInstructor = "Ramirez"
+
+const nombreCompleto = nombreInstructor + " " +  apellidoInstructor
+
+const nombreCompletoMejorado =`${nombreInstructor} ${apellidoInstructor}` 
+
+let edad =17;
+
+let fechaNacimiento = prompt("Indicame tu fecha de nacimiento:")
+
+let edadUsuario =  2025 - fechaNacimiento 
+
+alert("tu edad es: " + edadUsuario)
+
+edad = 18
+
+console.log("mi edad es:" + edad)
+
+*/
+
+// primer ejercicio:
+
+const nombreProducto = "Remera";
+
+const precioUnitario = 5
+
+const cantidadDeseada =  prompt("cuantos productos "+ "queres ?" + "el producto " + nombreProducto +  "vale " + precioUnitario)
+
+const total = precioUnitario * cantidadDeseada
+
+alert("su costo total que tiene que pagar es: " + total)
